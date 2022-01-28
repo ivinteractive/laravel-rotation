34d35b66d5c6ef836cd8a23b6278ea516b0e3286
iv-craig <cs@ivinteractive.com>
2022-01-28 16:57:37 -0500

    Add testing around decryption exceptions

--- tests/Unit/RotaterTest.php
+++ tests/Unit/RotaterTest.php
@@ -2,8 +2,13 @@
 
 namespace IvInteractive\Rotation\Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use IvInteractive\Rotation\Tests\Resources\User;
+
 class RotaterTest extends \IvInteractive\Rotation\Tests\TestCase
 {
+    use DatabaseMigrations;
+
     private $rotater;
 
     public function setUp(): void
@@ -25,5 +30,30 @@ class RotaterTest extends \IvInteractive\Rotation\Tests\TestCase
     public function testRotaterColumn()
     {
         $this->assertSame('dob', $this->rotater->getColumn());
+    }
+
+    public function testDoesNotReencryptTwice()
+    {
+        $oldEncrypter = $this->rotater->getOldEncrypter();
+
+        $user = User::factory()->create([
+            'dob' => $oldEncrypter->encrypt('2000-01-01'),
+        ]);
+
+        $reencrypted = $this->rotater->reencrypt($user->dob);
+        $rereencrypted = $this->rotater->reencrypt($reencrypted);
+
+        $this->assertSame($reencrypted, $rereencrypted);
+    }
+
+    public function testThrowsDecryptException()
+    {
+        $this->expectException(\Illuminate\Contracts\Encryption\DecryptException::class);
+
+        $user = User::factory()->create([
+            'dob' => '2000-01-01',
+        ]);
+
+        $this->rotater->reencrypt($user->dob);
     }
 }
